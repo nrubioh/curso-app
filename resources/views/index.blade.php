02362cd5940569ab3edcd5437c587f97d4979cae
nrubioh <nrubiohoskins@gmail.com>
2023-12-03 18:25:05 -0300

    adding create and index .blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,55 @@
+{{-- THE LIST IS HERE! --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1>New index view created by function index in -> 'class GamesController'</h1>
+    <p><a href="{{route ('gamesCreate')}}">Link to /games/create.</a></p>
+    <h2>Welcome to GamesList</h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>CATEGORY-ID</th>
+                <th>CREATED</th>
+                <th>ACTIVE</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($video_game_list_html as $item)
+                <tr style="text-align: center">  
+                    <td>{{ $item->id }}</td>
+                    <td><a href="{{route('gamesView',$item->id)}}">{{$item->name}}</a></td>
+                    <td>{{ $item->category_id }}</td>
+                    <td>{{ $item->created_at }}</td>
+                    <td>
+                        @if ( $item->active )
+                            <span style="color: rgb(32, 203, 32)">ACTIVE</span>
+                        @else
+                            <span style="color: red">INACTIVE</span>
+                        @endif
+                    </td>
+                </tr>
+            @empty
+                <tr style="text-align: center">
+                    <td>There are not games currently</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+{{-- old forelse --
+    @forelse ($video_game_list_html as $item)
+        <ul>
+            <li>{{ $item }}</li>
+        </ul>
+    @empty
+        <li>There are not games currently</li>
+    @endforelse 
+--}}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,32 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GamesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    //return view('welcome');
-    return "check message from route web.php"; 
+    //return "check message from route web.php"; 
+    return view('welcome');
 });
 
-Route::get('/games', function(){
-    return "welcome to the game list";
-});
+Route::get('/games', [GamesController::class, 'index'])->name('gamesIndex');
+/* 
+    Route::get('/games', function(){
+        return "welcome to the game list";
+    }); 
+*/
+Route::get('/games/create', [GamesController::class, 'create'])->name('gamesCreate');
+Route::get('/games/{name_game}/{category?}', [GamesController::class, 'help']);
+/*
+|------------------------------------------------------------------------------------
+| OPTIONAL VARIABLE => USE "?" AFTER THE VAR AND "= null" IN THE FUNCTION
+|------------------------------------------------------------------------------------
+| Route::get('/games/{name_game}/{category?}', function($name_game,$category = null){
+|  if ($category) {
+|      return "welcome to $name_game, this game belongs to $category";
+|  }else{
+|      return "welcome to $name_game";
+|       }
+|  });
+*/
+Route::post('/games/storeVideogame', [GamesController::class, 'storeVideogame'])->name('gamesStoreVideogame');
+Route::get('/view/{id_game}', [GamesController::class, 'view'])->name('gamesView');
+Route::post('/games/update', [GamesController::class, 'updateVideogame'])->name('gamesUpdateVideogame');
 
-
-//OPTIONAL VARIABLE => USE "?" AFTER THE VAR AND "= null" IN THE FUNCTION
-Route::get('/games/{name_game}/{category?}', function($name_game,$category = null){
-    if ($category) {
-        return "welcome to $name_game, this game belongs to $category";
-    }else{
-        return "welcome to $name_game";
-    }
-});
